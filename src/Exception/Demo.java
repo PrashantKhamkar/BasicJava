32faf214603f49d5a1f636ebac4f492a19cc5338
PrashantKhamkar <prashant.khamkar21@gmail.com>
2025-07-06 13:18:55 +0530

    Demo example of exception

---
+++ src/Exception/Demo.java
@@ -0,0 +1,13 @@
+package Exception;
+
+import java.sql.SQLOutput;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        System.out.println("In main ");
+
+        System.out.println("Thread name:-->"+Thread.currentThread().getName());
+    }
+}
