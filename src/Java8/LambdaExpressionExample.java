5f221e765f79584c92db74dfbad358be70209cd7
PrashantKhamkar <prashant.khamkar21@gmail.com>
2025-07-06 18:31:39 +0530

    Lambda expression example

---
+++ src/Java8/LambdaExpressionExample.java
@@ -0,0 +1,16 @@
+package Java8;
+
+@FunctionalInterface
+interface Employee{
+    String getName(); //Abstract method
+}
+
+
+public class LambdaExpressionExample {
+
+    public static void main(String[] args) {
+
+        Employee e =()->"Software Engineering"; //Interface reference can be used to hold the lambda expression
+        System.out.println(e.getName());
+    }
+}
